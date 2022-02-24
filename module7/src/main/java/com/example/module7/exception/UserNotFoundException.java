344ea6bba9cb3e53fb8b778363c4d4e3dbc21d3f
Nurgun Makarov <76607958+Rvze@users.noreply.github.com>
2022-02-24 18:36:22 +0300

    Delete module7 directory

--- module7/src/main/java/com/example/module7/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nurgunmakarov.module5.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
